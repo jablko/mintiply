28f1105b99552099df5668ed1726e49f661a4f22
Jack Bates <jack@nottheoilrig.com>
2012-08-12 06:29:26 -0700

    Use byte ranges to overcome maximum response size

--- mintiply.py
+++ mintiply.py
@@ -14,9 +14,24 @@ try:
   object, = Object.gql('WHERE url = :1', url)
 
 except ValueError:
-  result = urlfetch.fetch(url)
+  m = hashlib.sha256()
 
-  object = Object(digest=hashlib.sha256(result.content).digest(), url=url)
+  result = urlfetch.fetch(url,
+    allow_truncated=True)
+
+  m.update(result.content)
+
+  firstBytePos = len(result.content)
+  while result.content_was_truncated:
+    result = urlfetch.fetch(url,
+      allow_truncated=True,
+      headers={ 'Range': 'bytes={}-'.format(firstBytePos) })
+
+    m.update(result.content)
+
+    firstBytePos += len(result.content)
+
+  object = Object(digest=m.digest(), url=url)
   object.put()
 
 print 'Digest: SHA-256=' + base64.b64encode(object.digest)
