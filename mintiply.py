a9a6f6a0c90bcfc681076faf6ccd58ed77610c74
Jack Bates <jack@nottheoilrig.com>
2012-08-12 05:18:31 -0700

    Store digest with Datastore

--- mintiply.py
+++ mintiply.py
@@ -1,11 +1,23 @@
 import base64, hashlib, os
 from google.appengine.api import urlfetch
+from google.appengine.ext import db
+
+class Object(db.Model):
+  digest = db.ByteStringProperty()
+  url = db.LinkProperty()
 
 url = os.environ['PATH_INFO'][1:]
 if os.environ['QUERY_STRING']:
   url += '?' + os.environ['QUERY_STRING']
 
-result = urlfetch.fetch(url)
+try:
+  object, = Object.gql('WHERE url = :1', url)
 
-print 'Digest: SHA-256=' + base64.b64encode(hashlib.sha256(result.content).digest())
+except ValueError:
+  result = urlfetch.fetch(url)
+
+  object = Object(digest=hashlib.sha256(result.content).digest(), url=url)
+  object.put()
+
+print 'Digest: SHA-256=' + base64.b64encode(object.digest)
 print 'Location: ' + url
