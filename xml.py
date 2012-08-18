eb0a5aefd19fd7f36115fde758f80c5c344b9b81
Jack Bates <jack@nottheoilrig.com>
2012-08-18 04:24:23 -0700

    Only add filename* extended parameter if necessary

--- xml.py
+++ xml.py
@@ -1,17 +1,21 @@
 import re, unicodedata, urllib
 from mintiply import Object, object, token, url
 
-# The normal form KD (NFKD) will apply the compatibility decomposition, i.e.
-# replace all compatibility characters with their equivalents
-
-# What I *really* want is "iconv -t ASCII//TRANSLIT", why was the iconv module
-# dropped from Python 2.3?
-
-try:
-  filename = unicodedata.normalize('NFKD', object.name).encode('ascii', 'ignore')
-
-except TypeError:
+m = re.match('[\t !-~]*$', object.name)
+if m:
   filename = object.name
+  extParameter = ''
+
+else:
+
+  # The normal form KD (NFKD) will apply the compatibility decomposition, i.e.
+  # replace all compatibility characters with their equivalents
+
+  # What I *really* want is "iconv -t ASCII//TRANSLIT", why was the iconv module
+  # dropped from Python 2.3?
+
+  filename = unicodedata.normalize('NFKD', object.name).encode('ascii', 'ignore')
+  extParameter = '; filename*=utf-8\'\'{}.meta4'.format(urllib.quote(object.name.encode('utf-8'), '!#$&+^`|~'))
 
 m = re.match(token + '$', filename)
 if m:
@@ -20,7 +24,7 @@ if m:
 else:
   filename = '"{}.meta4"'.format(filename.replace('"', '\\"'))
 
-print 'Content-Disposition: filename={}; filename*=utf-8\'\'{}.meta4'.format(filename, urllib.quote(object.name.encode('utf-8'), '!#$&+^`|~'))
+print 'Content-Disposition: filename={}{}'.format(filename, extParameter)
 
 print 'Content-Type: application/metalink4+xml'
 
